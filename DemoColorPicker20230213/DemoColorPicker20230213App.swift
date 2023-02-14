9f04d797b72fe32cc864fb2c638f114071943f7d
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-13 20:43:36 -0500

    Initial Commit

---
+++ DemoColorPicker20230213/DemoColorPicker20230213App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoColorPicker20230213App.swift
+//  DemoColorPicker20230213
+//
+//  Created by Xavier on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoColorPicker20230213App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
